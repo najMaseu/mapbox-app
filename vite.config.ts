7f349650bc9fbd403fdbcdc1fbad89003a35acb0
Maseu <m.wachowicz@protonmail.com>
2023-04-21 01:50:52 +0200

    change main font and add svgs handling

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import legacy from '@vitejs/plugin-legacy';
 import eslint from 'vite-plugin-eslint';
 import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     react(),
+    svgr(),
     legacy({
       targets: ['ie >= 11'],
     }),
